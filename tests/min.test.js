ded5d3b473a6126f1d124fe5bc3aa594c4eff0fa
Elie Andraos <elie@andraos.org>
2020-05-06 21:23:08 +0300

    :white_check_mark: Add min rule tests

---
+++ tests/min.test.js
@@ -0,0 +1,34 @@
+import {min} from "../src";
+import {expect} from "@jest/globals";
+
+test('passes with numeric values and valid conditions', () => {
+    expect(min(10, 5)).toBe(true);
+    expect(min(12.5, 2.3)).toBe(true);
+});
+
+test('fails with numeric values and invalid conditions', () => {
+    expect(min(10, 50)).toBe(false);
+    expect(min(12.5, 25.3)).toBe(false);
+});
+
+test('passes with string and valid conditions', () => {
+    expect(min('foo-bar', 5)).toBe(true);
+});
+
+test('fails with string and invalid conditions', () => {
+    expect(min('foo-bar', 50)).toBe(false);
+});
+
+test('fails with array', () => {
+    expect(min([1, 2], 5)).toBe(false);
+});
+
+test('fails with object', () => {
+    expect(min({1: 1}, 2)).toBe(false);
+});
+
+test('fails with empty values', () => {
+    expect(min(null, 1)).toBe(false);
+    expect(min('', 1)).toBe(false);
+    expect(min(undefined, 1)).toBe(false);
+});
