b46bd9979e43bc4f2a8172b45cbc914590682efa
Elie Andraos <elie@andraos.org>
2020-05-06 22:30:08 +0300

    :white_check_mark: Add is_in rule test

---
+++ tests/is_in.test.js
@@ -0,0 +1,24 @@
+import {is_in} from "../src";
+import {expect} from "@jest/globals";
+
+test('passes with array and valid condition', () => {
+    expect(is_in('foo', ['foo', 'bar'])).toBe(true);
+});
+
+test('passes with comma separated string and valid condition', () => {
+    expect(is_in('foo', 'foo,bar')).toBe(true);
+});
+
+test('fails with array and invalid condition', () => {
+    expect(is_in('hello', ['foo', 'bar'])).toBe(false);
+});
+
+test('fails with comma separated string and invalid condition', () => {
+    expect(is_in('hello', 'foo,bar')).toBe(false);
+});
+
+test('fails with empty values', () => {
+    expect(is_in(null, null)).toBe(false);
+    expect(is_in('', '')).toBe(false);
+    expect(is_in(undefined, null)).toBe(false);
+});
