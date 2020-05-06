bed5c6c53b5fcb5544a95aa9a55d3f189d27eff5
Elie Andraos <elie@andraos.org>
2020-05-06 21:10:24 +0300

    :white_check_mark: Add integer rule

---
+++ tests/integer.test.js
@@ -0,0 +1,31 @@
+import {integer} from "../src";
+import {expect} from "@jest/globals";
+
+test('passes with valid numbers', () => {
+    expect(integer(1)).toBe(true);
+    expect(integer('1')).toBe(true);
+});
+
+test('fails with decimals', () => {
+    expect(integer(1.5)).toBe(false);
+    expect(integer('1.5')).toBe(false);
+})
+
+test('fails with strings', () => {
+    expect(integer('foo')).toBe(false);
+    expect(integer('123d')).toBe(false);
+});
+
+test('fails with array', () => {
+    expect(integer([1])).toBe(false);
+});
+
+test('fails with object', () => {
+    expect(integer({1: 1})).toBe(false);
+});
+
+test('fails with empty values', () => {
+    expect(integer(null)).toBe(false);
+    expect(integer('')).toBe(false);
+    expect(integer(undefined)).toBe(false);
+});
