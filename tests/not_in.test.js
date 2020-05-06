a607b5da6119fa5184e75f511471d9c3cae203c6
Elie Andraos <elie@andraos.org>
2020-05-06 22:30:38 +0300

    :white_check_mark: Add not_in rule test

---
+++ tests/not_in.test.js
@@ -0,0 +1,24 @@
+import {not_in} from "../src";
+import {expect} from "@jest/globals";
+
+test('passes with array and valid condition', () => {
+    expect(not_in('hello', ['foo', 'bar'])).toBe(true);
+});
+
+test('passes with comma separated string and valid condition', () => {
+    expect(not_in('hello', 'foo,bar')).toBe(true);
+});
+
+test('fails with array and invalid condition', () => {
+    expect(not_in('foo', ['foo', 'bar'])).toBe(false);
+});
+
+test('fails with comma separated string and invalid condition', () => {
+    expect(not_in('foo', 'foo,bar')).toBe(false);
+});
+
+test('passes with empty values', () => {
+    expect(not_in(null, null)).toBe(true);
+    expect(not_in('', '')).toBe(true);
+    expect(not_in(undefined, null)).toBe(true);
+});
