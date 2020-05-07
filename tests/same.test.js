85d5301cb2078430c03687948839f13d8a2e164f
Elie Andraos <elie@andraos.org>
2020-05-07 12:58:07 +0300

    :white_check_mark: Added same rule tests

---
+++ tests/same.test.js
@@ -0,0 +1,19 @@
+import {same} from "../src";
+import {expect} from "@jest/globals";
+
+test('passes with valid conditions', () => {
+    expect(same('foo', 'foo')).toBe(true);
+    expect(same(1, 1)).toBe(true);
+    expect(same(['foo', 'bar'], ['foo', 'bar'])).toBe(true);
+    expect(same({foo: 'bar'}, {foo: 'bar'})).toBe(true);
+});
+
+test('fails with invalid conditions', () => {
+    expect(same('foo', 'bar')).toBe(false);
+    expect(same(10, 20)).toBe(false);
+    expect(same(1, '1')).toBe(false);
+    expect(same(1, 'foo')).toBe(false);
+    expect(same(1, '1')).toBe(false);
+    expect(same(['foo', 'bar'], ['bar', 'foo'])).toBe(false);
+    expect(same({foo: 'bar'}, {bar: 'bar'})).toBe(false);
+});
