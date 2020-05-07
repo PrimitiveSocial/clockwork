e81686433d5c0e492302c77bfa7aba4168d4f159
Elie Andraos <elie@andraos.org>
2020-05-07 18:47:18 +0300

    :white_check_mark: Add ends_with rule test

---
+++ tests/ends_with.test.js
@@ -0,0 +1,16 @@
+import {ends_with} from "../src";
+import {expect} from "@jest/globals";
+
+test('passes with valid conditions', () => {
+    expect(ends_with('foo', 'o')).toBe(true);
+});
+
+test('fails with invalid conditions', () => {
+    expect(ends_with('foo', 'b')).toBe(false);
+    expect(ends_with(10, 0)).toBe(false);
+    expect(ends_with(['foo', 'bar'], 'bar')).toBe(false);
+    expect(ends_with({foo: 'bar'}, 'bar')).toBe(false);
+    expect(ends_with('', 'a')).toBe(false);
+    expect(ends_with(null, 'a')).toBe(false);
+    expect(ends_with(undefined, 'a')).toBe(false);
+});
