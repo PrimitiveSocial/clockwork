4d3ea44e17b76ad76259f4dd4ab8dea43c155e9d
Elie Andraos <elie@andraos.org>
2020-05-07 13:00:55 +0300

    :white_check_mark: Added different rule tests

---
+++ tests/different.test.js
@@ -0,0 +1,19 @@
+import {different} from "../src";
+import {expect} from "@jest/globals";
+
+test('passes with valid conditions', () => {
+    expect(different('foo', 'bar')).toBe(true);
+    expect(different(10, 20)).toBe(true);
+    expect(different(1, '1')).toBe(true);
+    expect(different(1, 'foo')).toBe(true);
+    expect(different(1, '1')).toBe(true);
+    expect(different(['foo', 'bar'], ['bar', 'foo'])).toBe(true);
+    expect(different({foo: 'bar'}, {bar: 'bar'})).toBe(true);
+});
+
+test('fails with invalid conditions', () => {
+    expect(different('foo', 'foo')).toBe(false);
+    expect(different(1, 1)).toBe(false);
+    expect(different(['foo', 'bar'], ['foo', 'bar'])).toBe(false);
+    expect(different({foo: 'bar'}, {foo: 'bar'})).toBe(false);
+});
