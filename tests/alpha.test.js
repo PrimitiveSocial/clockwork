4d88f955084c0688fb2f1ae51031fcd384fc60d2
Elie Andraos <elie@andraos.org>
2020-05-07 15:08:43 +0300

    :white_check_mark: Add alpha rule test

---
+++ tests/alpha.test.js
@@ -0,0 +1,17 @@
+import {alpha} from "../src";
+import {expect} from "@jest/globals";
+
+test('passes with valid condition', () => {
+    expect(alpha('foo')).toBe(true);
+});
+
+test('fails with invalid condition', () => {
+    expect(alpha('foo-bar!')).toBe(false);
+    expect(alpha('foobar23')).toBe(false);
+    expect(alpha(100)).toBe(false);
+    expect(alpha(['foo'])).toBe(false);
+    expect(alpha({foo: 'bar'})).toBe(false);
+    expect(alpha('')).toBe(false);
+    expect(alpha(null)).toBe(false);
+    expect(alpha(undefined)).toBe(false);
+});
