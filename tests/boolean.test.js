405da332500a8f86b3c7beb035c7b634f6c6b835
Elie Andraos <elie@andraos.org>
2020-05-07 18:46:59 +0300

    :white_check_mark: Add boolean test rule

---
+++ tests/boolean.test.js
@@ -0,0 +1,21 @@
+import {boolean} from "../src";
+import {expect} from "@jest/globals";
+
+test('passes with valid conditions', () => {
+    expect(boolean(false)).toBe(true);
+    expect(boolean(true)).toBe(true);
+});
+
+test('fails with invalid conditions', () => {
+    expect(boolean('foo')).toBe(false);
+    expect(boolean(1)).toBe(false);
+    expect(boolean(0)).toBe(false);
+    expect(boolean('true')).toBe(false);
+    expect(boolean('false')).toBe(false);
+    expect(boolean('0')).toBe(false);
+    expect(boolean(['foo'])).toBe(false);
+    expect(boolean({foo: 'bar'})).toBe(false);
+    expect(boolean('')).toBe(false);
+    expect(boolean(null)).toBe(false);
+    expect(boolean(undefined)).toBe(false);
+});
