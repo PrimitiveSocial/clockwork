3a7f8ef44907d8893c9d2e18af085b05516e0882
Elie Andraos <elie@andraos.org>
2020-05-07 18:17:52 +0300

    :white_check_mark: Add alpha_dash rule test

---
+++ tests/alpha_dash.test.js
@@ -0,0 +1,21 @@
+import {alpha_dash} from "../src";
+import {expect} from "@jest/globals";
+
+test('passes with valid condition', () => {
+    expect(alpha_dash('foo')).toBe(true);
+    expect(alpha_dash('-')).toBe(true);
+    expect(alpha_dash('_')).toBe(true);
+    expect(alpha_dash('_foo-bar')).toBe(true);
+});
+
+test('fails with invalid condition', () => {
+    expect(alpha_dash('foo-bar!')).toBe(false);
+    expect(alpha_dash('foobar23')).toBe(false);
+    expect(alpha_dash('10.75')).toBe(false);
+    expect(alpha_dash(100.25)).toBe(false);
+    expect(alpha_dash(['foo'])).toBe(false);
+    expect(alpha_dash({foo: 'bar'})).toBe(false);
+    expect(alpha_dash('')).toBe(false);
+    expect(alpha_dash(null)).toBe(false);
+    expect(alpha_dash(undefined)).toBe(false);
+});
